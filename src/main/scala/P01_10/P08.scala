00dea0a8d287bb828489d8f1296c1ad26e2fab10
KirariGlock <javafxscript20072011@gmail.com>
2017-10-23 20:21:09 +0900

    P08をリファクタリング

--- src/main/scala/P01_10/P08.scala
+++ src/main/scala/P01_10/P08.scala
@@ -4,17 +4,12 @@ object P08 extends App {
   val result = compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
   println(result)
 
-  private def compress(list: List[Symbol]):List[Symbol] = {
-    compressDetail(list.tail, list.head :: Nil)
-  }
-
-  private def compressDetail(targetList: List[Symbol], resultList: List[Symbol]): List[Symbol] ={
-    if(targetList.isEmpty){
-      resultList.reverse
-    } else if(resultList.head == targetList.head){
-      compressDetail(targetList.tail, resultList)
+  private def compress(targetList: List[Symbol], resultList: List[Symbol] = Nil):List[Symbol] = {
+    val (trueList, falseList) = targetList.span(_ == targetList.head)
+    if (falseList.isEmpty){
+      (trueList.head :: resultList).reverse
     } else {
-      compressDetail(targetList.tail, targetList.head :: resultList)
+      compress(falseList, trueList.head :: resultList)
     }
   }
 }
