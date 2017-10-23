45f83798ebe6f18cc0435ecfb220dbe93ac26454
KirariGlock <javafxscript20072011@gmail.com>
2017-10-23 20:24:15 +0900

    コメントを追加

--- src/main/scala/P01_10/P07.scala
+++ src/main/scala/P01_10/P07.scala
@@ -4,6 +4,11 @@ object P07 extends App {
   val result = flatten(List(List(1, 1), 2, List(3, List(5, 8))))
   println(result)
 
+  /**
+    * 与えられたList[Any]を良い感じに1つのリストにします。
+    * @param list 対象リスト
+    * @return 変換後のリスト
+    */
   private def flatten(list: List[Any]):List[Any] = {
     var resultList: List[Any] = List()
     for (value <- list){
--- src/main/scala/P01_10/P08.scala
+++ src/main/scala/P01_10/P08.scala
@@ -4,6 +4,12 @@ object P08 extends App {
   val result = compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
   println(result)
 
+  /**
+    * targetListの連続する要素を1つの要素にまとめたListを返します。
+    * @param targetList 対象リスト
+    * @param resultList 結果リスト(再起処理内部で使用します。)
+    * @return targetListの連続する要素を1つの要素にまとめたList
+    */
   private def compress(targetList: List[Symbol], resultList: List[Symbol] = Nil):List[Symbol] = {
     val (trueList, falseList) = targetList.span(_ == targetList.head)
     if (falseList.isEmpty){
