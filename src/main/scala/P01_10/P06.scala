915e4e109afdd639dd8791772c66e61bb28c1319
KirariGlock <javafxscript20072011@gmail.com>
2017-10-26 19:26:58 +0900

    回りくどい版を追加

--- src/main/scala/P01_10/P06.scala
+++ src/main/scala/P01_10/P06.scala
@@ -1,8 +1,18 @@
 package P01_10
 
 object P06 extends App {
-  val result = isPalindrome(List(1, 2, 3, 2, 1))
-  println(result)
+  println(isPalindromeSimple(List(1, 2, 3, 2, 1)))
+  println(isPalindrome(List(1, 2, 3, 2, 1)))
 
-  private def isPalindrome(list: List[Int]):Boolean = list == list.reverse
+  private def isPalindromeSimple(list: List[Int]):Boolean = list == list.reverse
+
+  private def isPalindrome(list: List[Int]):Boolean = list == reverse(list)
+
+  private def reverse(targetList: List[Int], resultList: List[Int] = Nil): List[Int] = {
+    if (targetList.isEmpty){
+      resultList
+    } else {
+      reverse(targetList.tail, targetList.head :: resultList)
+    }
+  }
 }
