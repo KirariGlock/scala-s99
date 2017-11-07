230344fccd72816e358aa86a2bfb75de78f755d1
KirariGlock <javafxscript20072011@gmail.com>
2017-11-07 20:36:17 +0900

    コメントを追加

--- src/main/scala/P01_10/P10.scala
+++ src/main/scala/P01_10/P10.scala
@@ -4,13 +4,9 @@ object P10 extends App {
   val result = encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
   println(result)
 
-  /**
-    * targetListに与えられた連続した文字列の個数をカウントして、その値と文字列のタプルをListにして返します。
-    * @param targetList 対象リスト
-    * @param resultList 結果リスト(再帰処理内部で使用します。)
-    * @return 結果リスト
-    */
+
   private def encode(targetList: List[Symbol], resultList: List[(Int, Symbol)] = Nil) :List[(Int, Symbol)] = {
+    // 仕様はあってるけど問題的にはP9を使わないとダメらしい…
     val (trueList, falseList) = targetList.span(_ == targetList.head)
     if (falseList.isEmpty){
       ((trueList.size, trueList.head) :: resultList).reverse
